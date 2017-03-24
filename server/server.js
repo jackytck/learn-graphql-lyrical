a7b493ae6062cc5ca6524e1ff066a70d047dc90c
Jacky Tang <jackytck@gmail.com>
2017-03-24 22:21:45 +0800

    :wrench: setup mongodb from mlab

--- server/server.js
+++ server/server.js
@@ -1,33 +1,35 @@
-const express = require('express');
-const models = require('./models');
-const expressGraphQL = require('express-graphql');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const schema = require('./schema/schema');
+require('dotenv').config()
+const express = require('express')
+// const models = require('./models')
+const expressGraphQL = require('express-graphql')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const schema = require('./schema/schema')
 
-const app = express();
+const app = express()
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+console.log(process.env)
+const MONGO_URI = process.env.MONGO_URI
 if (!MONGO_URI) {
-  throw new Error('You must provide a MongoLab URI');
+  throw new Error('You must provide a MongoLab URI')
 }
 
-mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.Promise = global.Promise
+mongoose.connect(MONGO_URI)
 mongoose.connection
     .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
+    .on('error', error => console.log('Error connecting to MongoLab:', error))
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use('/graphql', expressGraphQL({
   schema,
   graphiql: true
-}));
+}))
 
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('../webpack.config.js');
-app.use(webpackMiddleware(webpack(webpackConfig)));
+const webpackMiddleware = require('webpack-dev-middleware')
+const webpack = require('webpack')
+const webpackConfig = require('../webpack.config.js')
+app.use(webpackMiddleware(webpack(webpackConfig)))
 
-module.exports = app;
+module.exports = app
