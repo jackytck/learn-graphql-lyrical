a6a32a41064ef11fa41fb4375893cf11411ec0b4
Jacky Tang <jackytck@gmail.com>
2017-03-25 12:18:57 +0800

    :rotating_light: fix coding style

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './client/index.js',
@@ -25,4 +24,4 @@ module.exports = {
       template: 'client/index.html'
     })
   ]
-};
+}
