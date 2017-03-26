b7d57bb693ba801dfcd3c6d280d2279ea6eb0e80
Jacky Tang <jackytck@gmail.com>
2017-03-26 14:45:10 +0800

    :construction: use addSong mutation via react-apollo

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -3,27 +3,48 @@ import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 
 class SongCreate extends Component {
+  static propTypes = {
+    mutate: PropTypes.func
+  }
+
+  state = {
+    title: ''
+  }
+  _onSubmit = this.onSubmit.bind(this)
+
+  onSubmit (event) {
+    event.preventDefault()
+    this.props.mutate({
+      variables: {
+        title: this.state.title
+      }
+    })
+  }
+
   render () {
     return (
       <div>
         <h3>Create a New Song</h3>
-        <form />
+        <form onSubmit={this._onSubmit}>
+          <label>Song Title:</label>
+          <input
+            onChange={event => this.setState({ title: event.target.value })}
+            value={this.state.title}
+          />
+          <button type='submit'>Create</button>
+        </form>
       </div>
     )
   }
 }
 
-SongCreate.propTypes = {
-  data: PropTypes.object
-}
-
-const query = gql`
-  {
-    songs {
+const mutation = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
       id
       title
     }
   }
 `
 
-export default graphql(query)(SongCreate)
+export default graphql(mutation)(SongCreate)
