9a934ea3f032c4807fb04e3a7e9cb3d549aaff3f
Jacky Tang <jackytck@gmail.com>
2017-03-28 23:21:56 +0800

    :construction: fetch song detail via query variable from react router params

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,13 +1,31 @@
 import React, { Component, PropTypes } from 'react'
+import { graphql } from 'react-apollo'
+import fetchSong from '../queries/fetchSongDetail'
 
 class SongDetail extends Component {
+  static propTypes = {
+    data: PropTypes.shape({
+      loading: PropTypes.boolean,
+      song: PropTypes.object
+    })
+  }
+
   render () {
+    if (this.props.data.loading) {
+      return (
+        <div>Loading...</div>
+      )
+    }
+    const { song } = this.props.data
+
     return (
       <div>
-        <h3>Song Detail</h3>
+        <h3>{song.title}</h3>
       </div>
     )
   }
 }
 
-export default SongDetail
+export default graphql(fetchSong, {
+  options: props => ({ variables: { id: props.params.id } })
+})(SongDetail)
