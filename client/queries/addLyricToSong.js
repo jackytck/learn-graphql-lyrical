f7a8c4364a6a79c2b450323de47bd6528be3dc71
Jacky Tang <jackytck@gmail.com>
2017-03-29 22:45:09 +0800

    :wrench: AddLyricToSong mutation

---
+++ client/queries/addLyricToSong.js
@@ -0,0 +1,12 @@
+import gql from 'graphql-tag'
+
+export default gql`
+  mutation AddLyricToSong($content: String, $songId: ID) {
+    addLyricToSong(content: $content, songId: $songId) {
+      id
+      lyrics {
+        content
+      }
+    }
+  }
+`
