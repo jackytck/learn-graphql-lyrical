317e91905f10ae62ba134586229ad09484ad3f7e
Jacky Tang <jackytck@gmail.com>
2017-03-31 22:40:43 +0800

    :hammer: cache via dataIdFromObject

--- client/index.js
+++ client/index.js
@@ -10,7 +10,9 @@ import SongCreate from './components/SongCreate'
 import SongList from './components/SongList'
 import SongDetail from './components/SongDetail'
 
-const client = new ApolloClient()
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+})
 
 const Root = () => {
   return (
--- client/queries/addLyricToSong.js
+++ client/queries/addLyricToSong.js
@@ -5,6 +5,7 @@ export default gql`
     addLyricToSong(content: $content, songId: $songId) {
       id
       lyrics {
+        id
         content
       }
     }
