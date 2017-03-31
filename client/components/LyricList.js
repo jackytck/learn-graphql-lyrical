b1ac363465e7c9909d8ae531e4c3c88a5350d522
Jacky Tang <jackytck@gmail.com>
2017-03-31 22:47:41 +0800

    :construction: add like button

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -5,11 +5,21 @@ class LyricList extends Component {
     lyrics: PropTypes.array
   }
 
+  onLike (id) {
+    console.log(id)
+  }
+
   renderLyrics () {
     return this.props.lyrics.map(({ id, content }) => {
       return (
         <li key={id} className='collection-item'>
           {content}
+          <i
+            className='material-icons'
+            onClick={() => this.onLike(id)}
+          >
+            thumb_up
+          </i>
         </li>
       )
     })
